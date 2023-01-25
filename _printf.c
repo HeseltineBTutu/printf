4d05b7578a4636fa8557f9722900a34cafd1aff2
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-25 04:16:33 -0800

    #Issue: unresolved specifiers

--- _printf.c
+++ _printf.c
@@ -14,41 +14,52 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int i;
+	int count = 0;
+	int i, j;
+	char *s;
 
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++) {
-        if (format[i] == '%') {
-            i++;
-            if (format[i] == 'd' || format[i] == 'i') {
-                int num = va_arg(args, int);
-                if(num<0) 
-                {
-                    putchar('-');
-                    num = -num;
-                }
-                if(num>9) _printf("%d",num/10);
-                putchar(num%10+'0');
-                count++;
-            } else if (format[i] == 's') {
-                char *str = va_arg(args, char *);
-                while(*str)
-                {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            } else if (format[i] == '%') {
-                count += putchar('%');
-            } else {
-                count += putchar(format[i]);
-            }
-        } else {
-            count += putchar(format[i]);
-        }
-    }
-    va_end(args);
-    return count;
+	va_list args;
+	va_start(args, format);
+
+for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					s = va_arg(args, char *);
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					for (j = 0; s[j]; j++)
+					{
+						putchar(s[j]);
+						count++;
+					}
+					break;
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					/**
+					 * Handle invalid conversion.
+					 */
+					putchar((format[i]));
+					break;
+			}
+		}
+		else
+		{
+			write(1, &format[i], sizeof(char));
+			count++;
+		}
+	}
+	va_end(args);
+	return count;
 }
