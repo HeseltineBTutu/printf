40986d369b712ca89c9221e724f8ad94400a4b18
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-27 03:19:12 -0800

    #issue 5: fix

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 	signed int i;
 	char *s;
 	int count = 0;
-	void *p;
+	unsigned long int p;
 
 	va_list arg;
 	va_start(arg, format);
@@ -38,10 +38,10 @@ int _printf(const char *format, ...)
 					   write(1, "-", 1);
 					   count++;
 				   }
-				   count += print_string(convert(i, 10, 0));
+				   count += print_string(convert(i, 10));
 				   break;
 			case 'o' : i = va_arg(arg, unsigned int);
-				   count += print_string(convert(i, 8, 0));
+				   count += print_string(convert(i, 8));
 				   break;
 			case 's' : s = va_arg(arg, char *);
 				   if (!s)
@@ -50,57 +50,21 @@ int _printf(const char *format, ...)
 				   }
 				   count += print_string(s);
 				   break;
-			case 'i' :
-				   i = va_arg(arg, int);
-				   if (i < 0)
-				   {
-					   i = -i;
-					   write(1, "-", 1);
-					   count++;
-				   }
-				   count += print_string(convert(i, 10, 0));
-				   break;
-			case 'l' :
-				   i = va_arg(arg, long int);
-				   if (i <0)
-				   {
-					   i = -i;
-					   write(1, "-", 1);
-					   count++;
-				   }
-				   count +=print_string(convert(i, 10, 0));
-				   break;
-			case 'x' :
-				   i = va_arg(arg, unsigned int);
-				   count += print_string(convert(i, 16, 0));
-				   break;
-			case 'X' :
-				   i = va_arg(arg, unsigned int);
-				   count += print_string(convert(i, 16, 1));
-				   break;
-			case 'b' :
-				   _print_binary(va_arg(arg, unsigned int));
-				   break;
-			case 'u' :
-				   i = va_arg(arg, unsigned int);
-				   count += print_string(convert(i, 10, 0));
-				   break;
+			case 'x' : i = va_arg(arg, unsigned int );
+				   count += print_string(convert(i, 16));
+			           break;
 			case 'p':
-				   p = va_arg(arg, void *);
+				   p = (unsigned long int) va_arg(arg, void *);
 				   count += print_string("0x");
-				   count += print_string(convert((uintptr_t)p, 16, 0));
-				   break;
-			case '\0' :
-				   return (-1);
+				   count += print_string(convert(p, 16));
 				   break;
 			case '%':
 				   write(1, "%", 1);
 				   count++;
 				   break;
 			default:
-			   write(1, "%", 1);
 			   write(1, traverse, 1);
-			   count += 2;
+			   count++;
 			   break;
 		}
 	}
@@ -116,20 +80,17 @@ if (ret > 0)
 	count += ret;
 return (count);
 }
-char *convert(unsigned int num, int base, int uppercase)
+char *convert(int num, int base)
 {
-static char Representation[] = "0123456789abcdef";
+static char Representation[] = "01234456789ABCDEF";
 static char buffer[50];
 char *ptr;
-unsigned int count = 0;
-if (uppercase)
+int count = 0;
+int negative = 0;
+if (num < 0 && base == 10)
 {
-	Representation[10] = 'A';
-	Representation[11] = 'B';
-	Representation[12] = 'C';
-	Representation[13] = 'D';
-	Representation[14] = 'E';
-	Representation[15] = 'F';
+	negative = 1;
+	num = -num;
 }
 
 ptr = &buffer[49];
@@ -141,5 +102,10 @@ do
 	num /= base;
 }
 while (num != 0);
+
+if (negative)
+{
+	*--ptr = '-';
+}
 return (ptr);
 }
