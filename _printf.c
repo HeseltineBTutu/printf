d4150139aad343478ce0693de7ab37dfd3bf60b5
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-28 04:10:20 -0800

    new specifiers and helpers added

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - Receives the main string and all the necessary parameters to
  * print a formated string
@@ -7,16 +8,21 @@
  */
 int _printf(const char *format, ...)
 {
-
 	int printed_chars;
 	conver_t f_list[] = {
-
 		{"c", print_char},
-
 		{"s", print_string},
-
 		{"%", print_percent},
-
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
 	};
 	va_list arg_list;
 
--- formart_specifiers.c
+++ formart_specifiers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_char - Prints character
  * @list: list of arguments
@@ -39,3 +40,34 @@ int print_percent(__attribute__((unused))va_list list)
 	return (1);
 }
 
+/**
+ * print_integer - Prints an integer
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (print_unsigned_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsigned_number(num));
+}
